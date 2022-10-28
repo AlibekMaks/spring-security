8f2fc362e07015aad4202f35e0e8d65b8d2356ba
Alibek <a.maksut@platonus.kz>
2022-10-28 16:42:44 +0600

    Initial commit

---
+++ src/main/java/com/alibek/spring/security/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.alibek.spring.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
+
+    @GetMapping("/authenticated")
+    public String pageForAuthenticatedUsers() {
+        return "secured part of web service";
+    }
+}
