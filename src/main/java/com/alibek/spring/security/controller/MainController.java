25aa0b63a67720200c1cd523be619f1b9139ac7f
Alibek <a.maksut@platonus.kz>
2022-11-02 16:14:22 +0600

    role,user added

--- src/main/java/com/alibek/spring/security/controller/MainController.java
+++ src/main/java/com/alibek/spring/security/controller/MainController.java
@@ -1,10 +1,22 @@
 package com.alibek.spring.security.controller;
 
+import com.alibek.spring.security.entities.User;
+import com.alibek.spring.security.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class MainController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String homePage(){
@@ -12,7 +24,18 @@ public class MainController {
     }
 
     @GetMapping("/authenticated")
-    public String pageForAuthenticatedUsers() {
-        return "secured part of web service";
+    public String pageForAuthenticatedUsers(Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        return "secured part of web service " + user.getUsername() + " " + user.getEmail();
+    }
+
+    @GetMapping("/read_profile")
+    public String pageForReadProfile() {
+        return "page for read profile";
+    }
+
+    @GetMapping("/only_for_admins")
+    public String pageForOnlyForAdmins() {
+        return "admins page";
     }
 }
